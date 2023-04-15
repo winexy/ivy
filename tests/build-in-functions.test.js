8f2b32bdd85636be0f4d71c961686d1cd59e0462
Olzhas Seitmagambet <sei7magambet@gmail.com>
2023-04-15 16:15:03 +0600

    add comparison operators test

--- tests/build-in-functions.test.js
+++ tests/build-in-functions.test.js
@@ -3,7 +3,12 @@ import { test } from './test-util.js';
 export default ivy => {
   test(ivy, `(+ 1 5)`, 6);
   test(ivy, `(+ (+ 3 2) 5)`, 10);
-  test(ivy, `(+ 1 5)`, 6);
   test(ivy, `(+ (* 2 2) 6)`, 10);
-  test(ivy, `(/ (- 10 2) 2)`, 4);
+
+  test(ivy, `(< 1 5)`, true);
+  test(ivy, `(> 1 5)`, false);
+
+  test(ivy, `(>= 4 2)`, true);
+  test(ivy, `(<= 4 2)`, false);
+  test(ivy, `(= 4 4)`, true);
 };
